557c167d18e61b1481ebdbf3b1d2d57edc231ca4
jose-m-quiros <jqchaves1928@gmail.com>
2024-05-24 06:24:54 -0600

    Cambios finales de la F1

---
+++ js/monoplaza.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const logoElement = document.getElementById('logoImagen');
+
+  function cambiarLogo() {
+    if (window.innerWidth >= 927) {
+      logoElement.src = 'img/download.png';
+      logoElement.alt = 'Nuevo Logo';
+    } else {
+      logoElement.src = 'img/F1-in-Schools-Stretch-Black-scaled-e1608034793347.png';
+      logoElement.alt = 'Logo';
+    }
+  }
+
+  cambiarLogo();
+
+  window.addEventListener('resize', cambiarLogo);
+});
