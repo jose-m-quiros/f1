be3bf08c33c4a0de2e5cb9ce421cc286d90399e5
jose-m-quiros <jqchaves1928@gmail.com>
2024-05-25 18:44:22 -0600

    “Final”

--- js/monoplaza.js
+++ js/monoplaza.js
@@ -5,7 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
     if (window.innerWidth >= 927) {
       logoElement.src = 'img/download.png';
       logoElement.alt = 'Nuevo Logo';
-    } else {
+    } else if (window.innerWidth <= 700) {
+      logoImagen.src = 'img/download.png';
+      logoImagen.alt = 'Nuevo Logo';
+    }  else {
       logoElement.src = 'img/F1-in-Schools-Stretch-Black-scaled-e1608034793347.png';
       logoElement.alt = 'Logo';
     }
